3589707edf6d2c6c2d7293a3b89dac3bc8806cf8
Jakub Adler <adler.jakub@gmail.com>
2020-11-14 15:47:30 +0000

    feat: return false if password is not long enough

--- app/lib/password_strength/rules/length_rule.rb
+++ app/lib/password_strength/rules/length_rule.rb
@@ -1,2 +1,18 @@
-class LengthRule
+require 'password_strength/rules/base'
+
+module PasswordStrength
+  module Rules
+    class LengthRule < Base
+
+      def initialize(password:, option: nil, score: nil)
+        super
+      end
+
+      def passed?
+        false
+      end
+
+    end
+  end
 end
+
--- spec/app/lib/password_strength/rules/length_rule_spec.rb
+++ spec/app/lib/password_strength/rules/length_rule_spec.rb
@@ -1,8 +1,25 @@
-require 'password_strength/rules/base'
+require 'password_strength/rules/length_rule'
 
-RSpec.describe PasswordStrength::Rules::Base do
+RSpec.describe PasswordStrength::Rules::LengthRule do
+  subject { described_class }
 
-  it 'should ' do
+  let(:length_rule) { described_class.new(password: password, option: option) }
+  let(:password) { '' }
+  let(:option) { }
 
+  it { is_expected.to be < PasswordStrength::Rules::Base }
+
+  describe '#passed?' do
+    subject(:passed?) { length_rule.passed? }
+
+    context "with option is 6" do
+      let(:option) { 6 }
+
+      describe 'when password is "aaa"' do
+        let(:password) { 'a' * 3 }
+
+        it { is_expected.to be false }
+      end
+    end
   end
 end
